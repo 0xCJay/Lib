5e0cb99a8bcf2358b3629761fce89bddb6b643f8
0xCJ <cjayrbx@gmail.com>
2024-09-22 11:13:47 +0100

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'ThorHaxx'
+	SaveManager.Folder = 'LinoriaLibSettings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
